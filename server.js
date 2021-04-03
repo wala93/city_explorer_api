61afbe918253b615a4c44e274e392184bf73c5af
Walaa Alomari <walaahmad1987@gmail.com>
2021-04-03 16:36:51 +0300

    add movies**

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 3002;
 const superagent = require('superagent');
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
-
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
 
-const PARKS_API_KEY=process.env.PARKS_API_KEY;
+const PARKS_API_KEY = process.env.PARKS_API_KEY;
 
 
 // Allow access to our api from another domain
@@ -30,7 +30,7 @@ app.listen(PORT, () => console.log(` app listening on port ${PORT}!`));
 
 app.get('/location', handleLocation);
 
-function Locations (search_query, formatted_query, latitude, longitude) {
+function Locations(search_query, formatted_query, latitude, longitude) {
   this.search_query = search_query;
   this.formatted_query = formatted_query;
   this.latitude = latitude;
@@ -45,7 +45,7 @@ function handleLocation(request, response) {
   let city = request.query.city;
 
   const SQL = 'SELECT * FROM locations WHERE search_query = $1';
-  let sqlArr=[city];
+  let sqlArr = [city];
   client
     .query(SQL, sqlArr)
     .then((result) =>
@@ -54,14 +54,14 @@ function handleLocation(request, response) {
   const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`;
 
   console.log('inside location');
-  superagent.get(url).then( locationData => {
+  superagent.get(url).then(locationData => {
 
     const geoApiData = locationData.body[0];
     console.log(geoApiData);
     let location = new Locations(city, geoApiData.display_name, geoApiData.lat, geoApiData.lon);
     response.status(200).send(location);
 
-  }).catch((error) =>{
+  }).catch((error) => {
     response.send('Sorry, something went wrong');
   });
 
@@ -71,7 +71,7 @@ function handleLocation(request, response) {
 
 app.get('/weather', handlelWeather);
 
-function Forcast (dayweather) {
+function Forcast(dayweather) {
   // this.search_query = dayweather.search_query;
   this.forecast = dayweather.weather.description;
   this.time = dayweather.datetime;
@@ -80,7 +80,7 @@ function Forcast (dayweather) {
 
 
 function handlelWeather(request, response) {
-  let weatherJson= [];
+  let weatherJson = [];
 
   // let weatherData = require('./data/ weather.json');
   let search_query = request.query.search_query;
@@ -92,7 +92,7 @@ function handlelWeather(request, response) {
       console.log(search_query);
       weatherJson = weatherDta.body.data.map((dayweather) => {
         // weatherJson.push(new Forcast(dayweather));
-        return new Forcast (dayweather);
+        return new Forcast(dayweather);
 
         // return weatherJson;
       });
@@ -102,7 +102,7 @@ function handlelWeather(request, response) {
       // response.send(weatherDta.body.data);
 
 
-    }).catch((error) =>{
+    }).catch((error) => {
       response.send('Sorry, something went wrong');
     });
 
@@ -112,32 +112,63 @@ function handlelWeather(request, response) {
 
 
 
-app.get('/parks',handleParks);
+app.get('/parks', handleParks);
 
-function handleParks(request,respons){
-  const city=request.query.city;
-  const url=`https://developer.nps.gov/api/v1/parks?parkCode=${city}&api_key=${PARKS_API_KEY}`;
+function handleParks(request, respons) {
+  const city = request.query.city;
+  const url = `https://developer.nps.gov/api/v1/parks?parkCode=${city}&api_key=${PARKS_API_KEY}`;
 
 
-  const arrOfParks=[];
-  superagent.get(url).then(parksData=> {
+  const arrOfParks = [];
+  superagent.get(url).then(parksData => {
+    let data ;
+    data=parksData.data.map(park => {
+      arrOfParks.push( new Parks(park));
+      return arrOfParks;
 
-    parksData.data.map(park=>{
-      return new Parks(park);
     });
     console.log(arrOfParks);
-    respons.send(arrOfParks);
-  }).catch((error) =>{
+    respons.send(data);
+  }).catch((error) => {
     respons.send('Sorry, something went wrong');
   });
 
 }
 
 
-function Parks(data){
-  this.name=data.name;
-  this.address=data.address;
-  this.fee =data.fees;
-  this.description=data.description;
-  this.url=data.url;
+function Parks(data) {
+  this.name = data.name;
+  this.address = data.address;
+  this.fee = data.fees;
+  this.description = data.description;
+  this.url = data.url;
+}
+
+app.get('/movies', handlelMovies);
+
+function handlelMovies(request, response) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${request.query.city}`;
+  const moviesData = [];
+  superagent.get(url).then(resData => {
+    let data = resData.body.results.map(movie => {
+      moviesData.push(new Movie(movie));
+      return moviesData;
+    });
+    response.send(data);
+  }).catch((error) => {
+    response.send('Sorry, something went wrong');
+
+  });
+
+}
+function Movie (data){
+  this.title=data.title;
+  this.overview=data.overview;
+  this.average_votes=data.average_votes;
+  this.total_votes=data.total_votes;
+  this.image_url=`https://image.tmdb.org/t/p/w500${data.poster_path}`;
+  this.popularity=data.popularity;
+  this.released_on=data.released_on;
+
+
 }
