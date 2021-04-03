7adf77a26b74c204c0c03b3d7a66a476a710d403
Walaa Alomari <walaahmad1987@gmail.com>
2021-04-03 14:56:13 +0300

    hard try in DB

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
+const superagent = require('superagent');
 
 const app = express(); // Creates a server application.
-const PORT = process.env.PORT || 3002;
-const superagent = require('superagent');
+const PORT = process.env.PORT || 3000;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const PARKS_API_KEY = process.env.PARKS_API_KEY;
 
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL);
-
-const PARKS_API_KEY=process.env.PARKS_API_KEY;
+const { query } = require('express');
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client({
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 // Allow access to our api from another domain
 app.use(cors());
@@ -29,7 +37,7 @@ app.listen(PORT, () => console.log(` app listening on port ${PORT}!`));
 
 app.get('/location', handleLocation);
 
-function Locations (search_query, formatted_query, latitude, longitude) {
+function Locations(search_query, formatted_query, latitude, longitude) {
   this.search_query = search_query;
   this.formatted_query = formatted_query;
   this.latitude = latitude;
@@ -40,50 +48,44 @@ function Locations (search_query, formatted_query, latitude, longitude) {
 
 
 function handleLocation(request, response) {
-  // let city = request.query.city;
-  // let locationsData = require(`./data/location.json`);
-  // let locationObj ;
-  // try {
-  //   locationsData.forEach(value =>{
-  //     locationObj = new Locations(city,value.display_name, value.lat, value.lon);
-  //   });
-  //   response.status(200).json(locationObj);
-  // }
-  // catch (error){
-  //   response.status(500).send('ERROR');
 
-  // }
-
-  let city = request.query.city;
-
-  const SQL = 'SELECT * FROM locations WHERE search_query = $1';
-  let sqlArr=[city];
-  client
-    .query(SQL, sqlArr)
-    .then((result) =>
-      response.status(200).json(result.rows[0])
-    );
-  const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`;
 
   console.log('inside location');
-  superagent.get(url).then( locationData => {
+  let city = request.query.city;
+  const url = `https://eu1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
 
-    const geoApiData = locationData.body[0];
-    console.log(geoApiData);
-    let location = new Locations(city, geoApiData.display_name, geoApiData.lat, geoApiData.lon);
-    response.status(200).send(location);
+  let sqlArr = [city];
+  const SQL = 'SELECT * FROM locations WHERE search_query = $1';
 
-  }).catch((error) =>{
-    response.send('Sorry, something went wrong');
-  });
+  client
+    .query(SQL, sqlArr)
+    .then((result) => {
+      if (result.rowCount) {
+        console.log('from DB', result.rows);
+        response.status(200).json(result.rows[0]);
+      }
 
+      else {
+        superagent.get(url).then(locationData => {
 
+          const geoApiData = locationData.body[0];
+          const location = new Locations(city, geoApiData.display_name, geoApiData.lat, geoApiData.lon);
+          const safeValues = [city, locationData.body[0].display_name, locationData.body[0].lat, locationData.body[0].lon];
+          const sqlQuery = `INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4)`;
+          client.query(sqlQuery, safeValues)
+            .then((result) => {
+              console.log('from API');
+              response.status(200).send(location);
+            });
+        });
+      }
+    });
 }
 
 
 app.get('/weather', handlelWeather);
 
-function Forcast (dayweather) {
+function Forcast(dayweather) {
   // this.search_query = dayweather.search_query;
   this.forecast = dayweather.weather.description;
   this.time = dayweather.datetime;
@@ -92,7 +94,7 @@ function Forcast (dayweather) {
 
 
 function handlelWeather(request, response) {
-  let weatherJson= [];
+  let weatherJson = [];
 
   // let weatherData = require('./data/ weather.json');
   let search_query = request.query.search_query;
@@ -104,7 +106,7 @@ function handlelWeather(request, response) {
       console.log(search_query);
       weatherJson = weatherDta.body.data.map((dayweather) => {
         // weatherJson.push(new Forcast(dayweather));
-        return new Forcast (dayweather);
+        return new Forcast(dayweather);
 
         // return weatherJson;
       });
@@ -114,7 +116,7 @@ function handlelWeather(request, response) {
       // response.send(weatherDta.body.data);
 
 
-    }).catch((error) =>{
+    }).catch((error) => {
       response.send('Sorry, something went wrong');
     });
 
@@ -124,32 +126,32 @@ function handlelWeather(request, response) {
 
 
 
-app.get('/parks',handleParks);
+app.get('/parks', handleParks);
 
-function handleParks(request,respons){
-  const city=request.query.city;
-  const url=`https://developer.nps.gov/api/v1/parks?parkCode=${city}&api_key=${PARKS_API_KEY}`;
+function handleParks(request, respons) {
+  const city = request.query.city;
+  const url = `https://developer.nps.gov/api/v1/parks?parkCode=${city}&api_key=${PARKS_API_KEY}`;
 
 
-  const arrOfParks=[];
-  superagent.get(url).then(parksData=> {
+  const arrOfParks = [];
+  superagent.get(url).then(parksData => {
 
-    parksData.data.map(park=>{
+    parksData.data.map(park => {
       return new Parks(park);
     });
     console.log(arrOfParks);
     respons.send(arrOfParks);
-  }).catch((error) =>{
+  }).catch((error) => {
     respons.send('Sorry, something went wrong');
   });
 
 }
 
 
-function Parks(data){
-  this.name=data.name;
-  this.address=data.address;
-  this.fee =data.fees;
-  this.description=data.description;
-  this.url=data.url;
+function Parks(data) {
+  this.name = data.name;
+  this.address = data.address;
+  this.fee = data.fees;
+  this.description = data.description;
+  this.url = data.url;
 }
